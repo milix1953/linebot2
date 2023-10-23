1136af0ad822c9c5d328e96431319bc0feb202bd
milix1953 <79617515+milix1953@users.noreply.github.com>
2023-10-23 09:15:09 +0800

    Update message.py

--- message.py
+++ message.py
@@ -1,104 +1,6 @@
 #這些是LINE官方開放的套件組合透過import來套用這個檔案上
 from linebot.models import *
 
-#ImagemapSendMessage(組圖訊息)
-def imagemap_message():
-    message = ImagemapSendMessage(
-        base_url="https://i.imgur.com/BfTFVDN.jpg",
-        alt_text='最新的合作廠商有誰呢？',
-        base_size=BaseSize(height=2000, width=2000),
-        actions=[
-            URIImagemapAction(
-                #家樂福
-                link_uri="https://tw.shop.com/search/%E5%AE%B6%E6%A8%82%E7%A6%8F",
-                area=ImagemapArea(
-                    x=0, y=0, width=1000, height=1000
-                )
-            ),
-            URIImagemapAction(
-                #生活市集
-                link_uri="https://tw.shop.com/search/%E7%94%9F%E6%B4%BB%E5%B8%82%E9%9B%86",
-                area=ImagemapArea(
-                    x=1000, y=0, width=1000, height=1000
-                )
-            ),
-            URIImagemapAction(
-                #阿瘦皮鞋
-                link_uri="https://tw.shop.com/search/%E9%98%BF%E7%98%A6%E7%9A%AE%E9%9E%8B",
-                area=ImagemapArea(
-                    x=0, y=1000, width=1000, height=1000
-                )
-            ),
-            URIImagemapAction(
-                #塔吉特千層蛋糕
-                link_uri="https://tw.shop.com/search/%E5%A1%94%E5%90%89%E7%89%B9",
-                area=ImagemapArea(
-                    x=1000, y=1000, width=1000, height=500
-                )
-            ),
-            URIImagemapAction(
-                #亞尼克生乳捲
-                link_uri="https://tw.shop.com/search/%E4%BA%9E%E5%B0%BC%E5%85%8B",
-                area=ImagemapArea(
-                    x=1000, y=1500, width=1000, height=500
-                )
-            )
-        ]
-    )
-    return message
-
-#TemplateSendMessage - ButtonsTemplate (按鈕介面訊息)
-def buttons_message():
-    message = TemplateSendMessage(
-        alt_text='好消息來囉～',
-        template=ButtonsTemplate(
-            thumbnail_image_url="https://pic2.zhimg.com/v2-de4b8114e8408d5265503c8b41f59f85_b.jpg",
-            title="是否要進行抽獎活動？",
-            text="輸入生日後即獲得抽獎機會",
-            actions=[
-                DatetimePickerTemplateAction(
-                    label="請選擇生日",
-                    data="input_birthday",
-                    mode='date',
-                    initial='1990-01-01',
-                    max='2019-03-10',
-                    min='1930-01-01'
-                ),
-                MessageTemplateAction(
-                    label="看抽獎品項",
-                    text="有哪些抽獎品項呢？"
-                ),
-                URITemplateAction(
-                    label="免費註冊享回饋",
-                    uri="https://tw.shop.com/nbts/create-myaccount.xhtml?returnurl=https%3A%2F%2Ftw.shop.com%2F"
-                )
-            ]
-        )
-    )
-    return message
-
-#TemplateSendMessage - ConfirmTemplate(確認介面訊息)
-def Confirm_Template():
-
-    message = TemplateSendMessage(
-        alt_text='是否註冊成為會員？',
-        template=ConfirmTemplate(
-            text="是否註冊成為會員？",
-            actions=[
-                PostbackTemplateAction(
-                    label="馬上註冊",
-                    text="現在、立刻、馬上",
-                    data="會員註冊"
-                ),
-                MessageTemplateAction(
-                    label="查詢其他功能",
-                    text="查詢其他功能"
-                )
-            ]
-        )
-    )
-    return message
-
 #旋轉木馬按鈕訊息介面
 
 def Carousel_Template():
@@ -115,10 +17,10 @@ def Carousel_Template():
                             label='加入菜單',
                             data='新增菜單:清蒸鱈魚'
                         ),
-                        PostbackTemplateAction(
-                            label='移出菜單',
-                            data='刪除菜單:清蒸鱈魚'
-                        )
+                        # PostbackTemplateAction(
+                        #     label='移出菜單',
+                        #     data='刪除菜單:清蒸鱈魚'
+                        # )
                     ]
                 ),
                 CarouselColumn(
@@ -130,10 +32,10 @@ def Carousel_Template():
                             label='加入菜單',
                             data='新增菜單:清蒸鱈魚2'
                         ),
-                        PostbackTemplateAction(
-                            label='移出菜單',
-                            data='刪除菜單:清蒸鱈魚2'
-                        )
+                        # PostbackTemplateAction(
+                        #     label='移出菜單',
+                        #     data='刪除菜單:清蒸鱈魚2'
+                        # )
                     ]
                 )
             ]
@@ -141,43 +43,5 @@ def Carousel_Template():
     )
     return message
 
-#TemplateSendMessage - ImageCarouselTemplate(圖片旋轉木馬)
-def image_carousel_message1():
-    message = TemplateSendMessage(
-        alt_text='圖片旋轉木馬',
-        template=ImageCarouselTemplate(
-            columns=[
-                ImageCarouselColumn(
-                    image_url="https://i.imgur.com/uKYgfVs.jpg",
-                    action=URITemplateAction(
-                        label="新鮮水果",
-                        uri="http://img.juimg.com/tuku/yulantu/110709/222-110F91G31375.jpg"
-                    )
-                ),
-                ImageCarouselColumn(
-                    image_url="https://i.imgur.com/QOcAvjt.jpg",
-                    action=URITemplateAction(
-                        label="新鮮蔬菜",
-                        uri="https://cdn.101mediaimage.com/img/file/1410464751urhp5.jpg"
-                    )
-                ),
-                ImageCarouselColumn(
-                    image_url="https://i.imgur.com/Np7eFyj.jpg",
-                    action=URITemplateAction(
-                        label="可愛狗狗",
-                        uri="http://imgm.cnmo-img.com.cn/appimg/screenpic/big/674/673928.JPG"
-                    )
-                ),
-                ImageCarouselColumn(
-                    image_url="https://i.imgur.com/QRIa5Dz.jpg",
-                    action=URITemplateAction(
-                        label="可愛貓咪",
-                        uri="https://m-miya.net/wp-content/uploads/2014/07/0-065-1.min_.jpg"
-                    )
-                )
-            ]
-        )
-    )
-    return message
 
 #關於LINEBOT聊天內容範例
